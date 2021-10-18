7caece0c81d0b9ea374907d7ee9a0b9fbc44347f
Rohit Kumar Jha <rohitjha424@gmail.com>
2021-10-18 15:29:36 +0530

    First commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+import CreateTask from '../modals/createTask'
+import Card from './Card';
+
+const TodoList = () => {
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([])
+    
+    useEffect(() => {
+        let arr = localStorage.getItem("taskList")
+       
+        if(arr){
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+    }, [])
+
+
+    const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+    }
+
+
+    return (
+        <>
+            <div className = "header text-center">
+                <h3>Todo List</h3>
+                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Create Task</button>
+            </div>
+            <div className = "task-container">
+            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )}
+            </div>
+            <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
+            <div className = "footer text-center">
+                <h4 className = "pt-3">This Project is build using React</h4>
+            <span >
+                    Made with ❤️ by 
+                <a className = "pl-2" href="https://www.linkedin.com/in/rohitjha242/" target="_blank">Rohit Kumar Jha</a> 
+
+            </span>
+
+
+            </div>
+        </>
+    );
+};
+
+export default TodoList;
